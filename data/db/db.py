701fd18e1dd9238671ed7ad29ee72f9f8ca2b110
alexey-gm <gaidaialexey0502@gmail.com>
2025-04-02 23:55:31 +0700

    добавил комментарий к db.py

--- data/db/db.py
+++ data/db/db.py
@@ -3,6 +3,12 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "postgresql://postgres:admin@localhost/attendance_db"
 
+# создаем базу данных
+
+
+
+
+# создаем движок базы данных
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
